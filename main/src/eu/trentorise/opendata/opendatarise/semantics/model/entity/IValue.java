e9b1db637a9b7de075a9f251322404f19507f037
Moaz Reyad <moazreyad@gmail.com>
2013-07-31 14:52:15 +0200

    Remove Javadoc template

--- main/src/eu/trentorise/opendata/opendatarise/semantics/model/entity/IValue.java
+++ main/src/eu/trentorise/opendata/opendatarise/semantics/model/entity/IValue.java
@@ -16,10 +16,6 @@
  *******************************************************************************
  */
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package eu.trentorise.opendata.opendatarise.semantics.model.entity;
 
 /**
