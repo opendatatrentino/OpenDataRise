74352de89b444b37a14d18311e6318d75fc538c7
Moaz Reyad <moazreyad@gmail.com>
2013-08-01 14:16:42 +0200

    add NLP interface methods

--- main/src/eu/trentorise/opendata/opendatarise/semantics/services/INLP.java
+++ main/src/eu/trentorise/opendata/opendatarise/semantics/services/INLP.java
@@ -15,15 +15,57 @@
  *
  *******************************************************************************
  */
-
 package eu.trentorise.opendata.opendatarise.semantics.services;
 
+import eu.trentorise.opendata.opendatarise.semantics.model.knowledge.INLPText;
+
 /**
- * TODO same as the one defined by moaz
+ * The NLP interface provides natural language processing services such as named
+ * entity recognition and word sense disambiguation
  *
  * @author Juan Pane <pane@disi.unitn.it>
  * @author Moaz Reyad <reyad@disi.unitn.it>
  * @date Jul 24, 2013
  */
 public interface INLP {
+
+    /**
+     * Takes a natural language string and returns concepts and entities
+     * disambiguated
+     *
+     * @param nlText an input natural language string
+     * @return the natural language text object with all the entities and
+     * concepts found and disambiguated
+     */
+    INLPText RunNLP(String nlText);
+
+    /**
+     * Takes a natural language name of an entity as a string and finds the
+     * disambiguated entity.
+     *
+     * @param nlText the natural language name of an entity which has been
+     * processed before by Named Entity Recognition. After calling the function
+     * it will have also the disambiguated Entity added to the object with their
+     * locations.
+     */
+    void NamedEntityDisambigaute(INLPText nlText);
+
+    /**
+     * Takes a natural language text and finds the concepts and entities in the
+     * text.
+     *
+     * @param nlText the input natural language string. After calling the
+     * function it will have also the concept and entities found in the text.
+     * They will be added to the object with their locations.
+     */
+    void NamedEntityRecognition(INLPText nlText);
+
+    /**
+     * Takes a natural language text that has been processed by NER and
+     * disambiguates the concepts
+     *
+     * @param nlText the input natural language string. After calling the
+     * function it will have also the concepts disambiguated.
+     */
+    void WordSenseDisambigaute(INLPText nlText);
 }
