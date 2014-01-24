55ae36dc31cb4412376a81bd65b20735ec910770
David Leoni <davidleonigit-github@yahoo.com>
2014-01-24 14:09:19 +0100

    set CkanResource.lastModified and size as @Nullable

--- main/src/eu/trentorise/opendata/opendatarise/semantics/services/ISemanticMatchingService.java
+++ main/src/eu/trentorise/opendata/opendatarise/semantics/services/ISemanticMatchingService.java
@@ -18,6 +18,8 @@
 
 package eu.trentorise.opendata.opendatarise.semantics.services;
 
+import eu.trentorise.opendata.jackan.ckan.CkanDataset;
+import eu.trentorise.opendata.jackan.ckan.CkanResource;
 import eu.trentorise.opendata.opendatarise.semantics.model.entity.IEntityType;
 import eu.trentorise.opendata.opendatarise.semantics.services.model.ICorrespondence;
 import java.util.List;
@@ -27,21 +29,20 @@ import java.util.List;
  *
  * @author Juan Pane <pane@disi.unitn.it>
  * @author Moaz Reyad <reyad@disi.unitn.it>
- * @date Jul 24, 2013
+ * @author David Leoni <david.leoni@trentorise.eu>
+ * @date Jan 24, 2014
  */
 public interface ISemanticMatchingService {
 
-    /*
-     * Returns the correspondence between the source and the target contexts. 
-     * The first context is given by a root node and a list of columns. The 
-     * second context is given by the definition of an entity type
-     * 
-     * @param sourceName - The name of the root node in the source tree
-     * @param sourceNodes - Names of the source nodes under the source root node
-     * @param targetEntityType - the target entity type
-     * 
-     * @return - the correspondence
+    /**
+     * Given metadata and column headers names of an input resource of ckan, along with the url of the catalog of provenance, it guesses a target entitytype and returns a matching between the source headers and the attributes of the target entity type.
+     
+     * @param catalogUrl
+     * @param ckanDataset the dataset of the resource
+     * @param ckanResource a ckan resource
+     * @param sourceColumns the names of the resource columns
+     * @return  - the correspondence
      */
-    ICorrespondence matchSchemas(String sourceName, List<String> sourceColumns,
-            IEntityType targetEntityType);
+    
+    ICorrespondence matchSchemas(String catalogUrl, CkanDataset ckanDataset, CkanResource ckanResource, List<String> sourceColumns);
 }
